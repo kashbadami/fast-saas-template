72e34273751625212417a17e99f212df0c5eb4b5
Kash Badami <kash@badami.net>
2025-07-27 16:02:15 -0700

    fix: configure Next.js to allow external images from CDN
    - Added image domain configuration for deifkwefumgah.cloudfront.net
    - Added common OAuth provider image domains (Google, GitHub, Discord)
    - Created placeholder.svg for fallback image support
    - This fixes image loading issues in production deployment

--- next.config.js
+++ next.config.js
@@ -5,6 +5,36 @@
 import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'deifkwefumgah.cloudfront.net',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '*.googleusercontent.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.discordapp.com',
+        pathname: '/**',
+      },
+    ],
+  },
+};
 
 export default config;
